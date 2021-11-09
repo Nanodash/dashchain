afe1e0753f5e0062254a950446581b44db90a326
N4n0d4sh <nanodashdev@gmail.com>
2021-11-09 23:21:55 +0100

    fix singleton init + simple log + constants file and docs

--- lib/src/binance/binance_rest_api.dart
+++ lib/src/binance/binance_rest_api.dart
@@ -1,46 +1,60 @@
-import 'package:http/http.dart' as http;
-import 'dart:io';
 import 'dart:math';
+
+import 'package:http/http.dart' as http;
+
+import 'package:dashchain/src/binance/constants.dart';
 
 /// Based on official docs at https://github.com/binance-exchange/binance-official-api-docs
 class BinanceRestApi {
   static BinanceRestApi? _instance;
 
   factory BinanceRestApi() => _instance ?? BinanceRestApi._();
-  BinanceRestApi._();
-
-  static late final Stopwatch _stopwatch = Stopwatch();
-
-  static const String httpsScheme = 'https://';
-  static const String binanceDomain = 'binance.com';
-  static const String apiPath = '/api/v3';
-
-  /// The default endpoint to use when interacting with Binance Rest API
-  static String get baseEndpoint => '${httpsScheme}api.$binanceDomain$apiPath';
-
-  /// Will return a fallback endpoint if [baseEndpoint] has some performance or availability issues
-  static String get fallbackEndpoint {
-    final r = Random(baseEndpoint.length);
-    final apiNumber = r.nextInt(3) + 1; // get number in [1, 2, 3]
-    return '${httpsScheme}api$apiNumber.$binanceDomain$apiPath';
+  BinanceRestApi._() {
+    _instance = this;
+    _log('API manager initialized !');
   }
 
+  /// A simple log method (may integrate logging pkg later)
+  void _log(String msg) => print('[$runtimeType] $msg');
+
+  /// A [Stopwatch] to log requests duration (may remove before production use, or have an opt-in toggle)
+  static late final Stopwatch _stopwatch = Stopwatch();
+
+  /// Will reset [_stopwatch] and restart it
+  void _restartStopwatch() {
+    _stopwatch.reset();
+    _stopwatch.stop();
+    _stopwatch.start();
+  }
+
+  /// The default endpoint to use when interacting with Binance Rest API
+  String get baseEndpoint => 'https://api.$binanceDomain$apiPath';
+
+  /// Will return a fallback endpoint if [baseEndpoint] has some performance or availability issues
+  String get fallbackEndpoint {
+    final r = Random(baseEndpoint.length);
+    final apiNumber = r.nextInt(3) + 1; // get number in [1, 2, 3]
+    final fallbackEndpoint = 'https://api$apiNumber.$binanceDomain$apiPath';
+    _log('fallback to $fallbackEndpoint');
+    return fallbackEndpoint;
+  }
+
+  /// Will check for Binance's _REST API_ availability using `/ping` endpoint as recommended
   Future<bool> checkAvailability() async {
     var isApiAvailable = false;
-    _stopwatch.reset();
-    _stopwatch.start();
-    print('will ping API to check availability...');
+    _restartStopwatch();
+    _log('will ping API to check availability...');
     final response = await http.get(Uri.parse('$baseEndpoint/ping'));
     final code = response.statusCode;
-    print(
-        'received answer with code $code after ${_stopwatch.elapsedMilliseconds}ms.');
+    _log('received answer with code $code '
+        'after ${_stopwatch.elapsedMilliseconds}ms.');
     switch (code) {
       case 200:
         isApiAvailable = true;
         break;
       default:
-        print('received error code ${response.statusCode}');
-        print('reasonPhrase ${response.reasonPhrase}');
+        _log('received error code ${response.statusCode}');
+        _log('reasonPhrase ${response.reasonPhrase}');
         break;
     }
     return isApiAvailable;
---
+++ lib/src/binance/constants.dart
@@ -0,0 +1,5 @@
+/// Binance's base domain name
+const String binanceDomain = 'binance.com';
+
+/// The base path for Binance's _REST API_
+const String apiPath = '/api/v3';
