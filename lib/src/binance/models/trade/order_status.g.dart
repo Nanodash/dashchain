7a543d02827de9e1e3c0d4b4164ffdbdf10211d1
Nanodash <61005635+Nanodash@users.noreply.github.com>
2022-02-19 09:39:50 +0100

    Feature : get order status (#30)
    * first version of getOrderStatus method
    
    * implement BinanceOrderStatus freezed object and update related code
    
    * add getOrderStatus call in example script
    
    * bump pkg version and update CHANGELOG.md

---
+++ lib/src/binance/models/trade/order_status.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order_status.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_BinanceOrderStatus _$$_BinanceOrderStatusFromJson(
+        Map<String, dynamic> json) =>
+    _$_BinanceOrderStatus(
+      json['symbol'] as String,
+      json['orderId'] as int,
+      json['orderListId'] as int,
+      json['clientOrderId'] as String,
+      json['price'] as String,
+      json['origQty'] as String,
+      json['executedQty'] as String,
+      json['cummulativeQuoteQty'] as String,
+      json['status'] as String,
+      json['timeInForce'] as String,
+      json['type'] as String,
+      json['side'] as String,
+      json['stopPrice'] as String,
+      json['icebergQty'] as String,
+      json['time'] as int,
+      json['updateTime'] as int,
+      json['isWorking'] as bool,
+      json['origQuoteOrderQty'] as String,
+    );
+
+Map<String, dynamic> _$$_BinanceOrderStatusToJson(
+        _$_BinanceOrderStatus instance) =>
+    <String, dynamic>{
+      'symbol': instance.symbol,
+      'orderId': instance.orderId,
+      'orderListId': instance.orderListId,
+      'clientOrderId': instance.clientOrderId,
+      'price': instance.price,
+      'origQty': instance.origQty,
+      'executedQty': instance.executedQty,
+      'cummulativeQuoteQty': instance.cummulativeQuoteQty,
+      'status': instance.status,
+      'timeInForce': instance.timeInForce,
+      'type': instance.type,
+      'side': instance.side,
+      'stopPrice': instance.stopPrice,
+      'icebergQty': instance.icebergQty,
+      'time': instance.time,
+      'updateTime': instance.updateTime,
+      'isWorking': instance.isWorking,
+      'origQuoteOrderQty': instance.origQuoteOrderQty,
+    };
